8973c2130152b5ef636ee561571a63e1baadb13f
George Sykes <georgesykes86@gmail.com>
2018-06-17 18:43:20 +0100

    Removes array for chosen roomates

--- lib/data_cleaner.rb
+++ lib/data_cleaner.rb
@@ -11,7 +11,6 @@ class Datacleaner
   def clean
     num_students = @data.length
     selected_pairings = @data[0].length
-    points_allocation = #array of 5 points for the chosen mates
   end
 
   def zero_index
