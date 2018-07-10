23e5dff39b85caea8b4dbe319197d5864de37922
George Sykes <georgesykes86@gmail.com>
2018-07-10 13:24:59 +0100

    Adds zero_index to Datacleaner

--- lib/data_cleaner.rb
+++ lib/data_cleaner.rb
@@ -1,18 +1,7 @@
 class Datacleaner
 
-  attr_accessor :data
-
-  def initialize
-    @data = nil
-  end
-
-  def clean
-    num_students = @data.length
-    selected_pairings = @data[0].length
-  end
-
-  def zero_index
-    @data.map! { |array| array.map! { |number| number - 1 } }
+  def zero_index(data)
+    data.map! { |array| array.map! { |number| number - 1 } }
   end
 
 end
---
+++ spec/data_cleaner_spec.rb
@@ -0,0 +1,26 @@
+describe Datacleaner do
+  subject(:datacleaner) { described_class.new() }
+
+  non_zero_indexed_data = [
+    [2, 3, 4],
+    [1, 3, 4],
+    [1, 2, 4],
+    [1, 2, 3]
+  ]
+
+  zero_indexed_data = [
+    [1, 2, 3],
+    [0, 2, 3],
+    [0, 1, 3],
+    [0, 1, 2]
+  ]
+
+  describe 'zero_index' do
+
+    it 'zeroindexes the data when not zero indexed' do
+      expect(datacleaner.zero_index(non_zero_indexed_data)).to eq(zero_indexed_data)
+    end
+
+  end
+
+end
